5db91a41654361d4b32a348af0db1e662a297ec8
Arwa mohammed <arwa.elmowafy1996@gmail.com>
2022-06-16 21:16:40 +0200

    refactor: create home/search pages

---
+++ src/screens/HomePage.tsx
@@ -0,0 +1,125 @@
+import React, { useEffect, useState } from 'react';
+import * as BooksAPI from '../BooksAPI';
+import { useNavigate } from 'react-router-dom';
+import '../App.css';
+import { Book } from '../components/BookShelf/Book';
+import { BookShelf } from '../components/BookShelf/BookShelf';
+import { SearchBooks } from '../components/SearchBooks/SearchBooks';
+
+const HomePage = () => {
+  // state = {
+  //   /**
+  //    * TODO: Instead of using this state variable to keep track of which page
+  //    * we're on, use the URL in the browser's address bar. This will ensure that
+  //    * users can use the browser's back and forward buttons to navigate between
+  //    * pages, as well as provide a good URL they can bookmark and share.
+  //    */
+  //   showSearchPage: false,
+  // };
+  const navigate = useNavigate();
+  const [books, setBooks] = useState([]);
+  const [showSearchPage, setShowSearchPage] = useState<boolean>(false);
+
+  useEffect(() => {
+    BooksAPI.getAll()
+      .then((books) => {
+        setBooks(books);
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  }, []);
+
+  const updateBookShelf = (book: any, shelf: string) => {
+    console.log(book, shelf);
+    BooksAPI.update(book, shelf);
+  };
+  const currentlyReadingBooks = books
+    .filter((book: any) => book.shelf === 'currentlyReading')
+    .map((book) => book);
+
+  const wantToReadBooks = books
+    .filter((book: any) => book.shelf === 'wantToRead')
+    .map((book) => book);
+
+  const readBooks = books
+    .filter((book: any) => book.shelf === 'read')
+    .map((book) => book);
+
+  return (
+    <div className='app'>
+      {showSearchPage ? (
+        <SearchBooks />
+      ) : (
+        <div className='list-books'>
+          <div className='list-books-title'>
+            <h1>MyReads</h1>
+          </div>
+          <div className='list-books-content'>
+            <div>
+              <BookShelf title={'Currently Reading'}>
+                <ol className='books-grid'>
+                  {currentlyReadingBooks.map((book: any) => (
+                    <li>
+                      <Book
+                        bookTitle={book.title}
+                        bookAuthor={`Arwa`}
+                        URL={`url(${book.imageLinks.smallThumbnail})`}
+                        book={book}
+                        updateBookShelf={updateBookShelf}
+                      />
+                    </li>
+                  ))}
+                </ol>
+              </BookShelf>
+
+              <BookShelf title={'Want to Read'}>
+                <ol className='books-grid'>
+                  {wantToReadBooks.map((book: any) => (
+                    <li>
+                      <Book
+                        bookTitle={book.title}
+                        bookAuthor={`Arwa`}
+                        URL={`url(${book.imageLinks.smallThumbnail})`}
+                        book={book}
+                        updateBookShelf={updateBookShelf}
+                      />
+                    </li>
+                  ))}
+                </ol>
+              </BookShelf>
+
+              <BookShelf title={'Read'}>
+                <ol className='books-grid'>
+                  {readBooks.map((book: any) => (
+                    <li>
+                      <Book
+                        bookTitle={book.title}
+                        bookAuthor={`Arwa`}
+                        URL={`url(${book.imageLinks.smallThumbnail})`}
+                        book={book}
+                        updateBookShelf={updateBookShelf}
+                      />
+                    </li>
+                  ))}
+                </ol>
+              </BookShelf>
+            </div>
+          </div>
+          <div className='open-search'>
+            <button
+              onClick={() => {
+                setShowSearchPage(true);
+                navigate('/search');
+              }}
+            >
+              Add a book
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
